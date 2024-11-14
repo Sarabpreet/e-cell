5564f1de886c7e3fcaedced3f9ae3873d6231fff
Ahmed S. <84773790@users.noreply.github.com>
2024-11-14 11:02:14 +0000

    Interactive Image component created
    3d interactive image mnade for homepage hero section.

---
+++ frontend/src/components/interactiveImgHome.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+
+const InteractiveImage = ({ source, alt, className }) => {
+    const [transform, setTransform] = useState("translateZ(0)");
+
+    const handleMouseMove = (e) => {
+        const { offsetWidth, offsetHeight } = e.currentTarget;
+        const { offsetX, offsetY } = e.nativeEvent;
+
+        const rotateX = ((offsetY - offsetHeight / 2) / offsetHeight * 20);
+        const rotateY = ((offsetX - offsetWidth / 2) / offsetWidth * -20);
+
+        setTransform(`perspective(1000px) translateZ(-20px) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`);
+    }
+
+    const handleMouseLeave = () => setTransform('translateZ(0)');
+
+    return (<img
+        src={source}
+        alt={alt}
+        className={className}
+        onMouseMove={handleMouseMove}
+        onMouseLeave={handleMouseLeave}
+        style={{ transform: transform }}
+    />);
+}
+
+export default InteractiveImage;
