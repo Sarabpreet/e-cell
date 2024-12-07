63e8559e789449b0209f09ae132fef51a417dcf4
Ahmed S. <84773790@users.noreply.github.com>
2024-12-07 05:56:01 +0000

    Included custom color palette in Tailwind

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,7 +4,14 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "blue-dark": "#01132B",
+        "blue-mid": "#106B88",
+        "blue-light": "#64CDC6",
+        "blue-pale": "#DAFEFA"
+      }
+    },
   },
   plugins: [],
 }
