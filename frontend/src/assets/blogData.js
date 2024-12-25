20835a50179e1406e5b1c2a11c6e529f03f9b60d
Ahmed S. <s.ahmed.profiquence@gmail.com>
2024-12-25 12:00:04 +0530

    Updated links for all the newsletters(blogs) available

--- frontend/src/assets/blogData.js
+++ frontend/src/assets/blogData.js
@@ -2,26 +2,41 @@ export const BlogData = [
     {
         image: "https://picsum.photos/400/200",
         title: "Lorem Ipsum Blog Heading",
-        postURL: "https://example.com/"
+        postURL: "https://www.linkedin.com/pulse/startup-scoop-edition-16-ecellmit-dni7f"
     },
     {
         image: "https://picsum.photos/400/200",
         title: "Lorem Ipsum Blog Heading",
-        postURL: "https://example.com/"
+        postURL: "https://www.linkedin.com/pulse/startup-scoop-edition-15-ecellmit-sew8f"
     },
     {
         image: "https://picsum.photos/400/200",
         title: "Lorem Ipsum Blog Heading",
-        postURL: "https://example.com/"
+        postURL: "https://www.linkedin.com/pulse/startup-scoop-edition-14-ecellmit-fbycf"
     },
     {
         image: "https://picsum.photos/400/200",
         title: "Lorem Ipsum Blog Heading",
-        postURL: "https://example.com/"
+        postURL: "https://www.linkedin.com/pulse/startup-scoop-edition-13-ecellmit-evqof"
     },
     {
         image: "https://picsum.photos/400/200",
         title: "Lorem Ipsum Blog Heading",
-        postURL: "https://example.com/"
+        postURL: "https://www.linkedin.com/pulse/startup-scoop-edition-12-ecellmit-ewm4f"
+    },
+    {
+        image: "https://picsum.photos/400/200",
+        title: "Lorem Ipsum Blog Heading",
+        postURL: "https://www.linkedin.com/pulse/startup-scoop-edition-11-ecellmit-jkh3f"
+    },
+    {
+        image: "https://picsum.photos/400/200",
+        title: "Lorem Ipsum Blog Heading",
+        postURL: "https://www.linkedin.com/pulse/startup-scoop-edition-10-ecellmit-iyp7f"
+    },
+    {
+        image: "https://picsum.photos/400/200",
+        title: "Lorem Ipsum Blog Heading",
+        postURL: "https://www.linkedin.com/pulse/startup-scoop-edition-9-ecellmit-uddzc"
     }
 ];
