cdb464190818ec8e525e58867260424518940c77
Ahmed S. <s.ahmed.profiquence@gmail.com>
2025-01-02 22:01:32 +0530

    Revamped MES Page Design

--- frontend/src/pages/mes.js
+++ frontend/src/pages/mes.js
@@ -3,6 +3,8 @@ import './vc.css';
 import vc_photo from './bg.jpeg';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
+import { BlogList } from '../components/blogCard';
+
 import { vcList, panIndiaEvents, maheEvents, flagshipEvents } from '../assets/mesData';
 
 function EventSection({ title, events, gradient }) {
@@ -19,9 +21,8 @@ function EventSection({ title, events, gradient }) {
         {events.map((event, index) => (
           <div
             key={index}
-            className={`p-6 bg-transparent border border-white rounded-lg shadow-lg h-full cursor-pointer transform transition-all duration-300 ease-in-out ${
-              activeIndex === index ? 'scale-105' : ''
-            }`}
+            className={`p-6 bg-transparent border border-white rounded-lg shadow-lg h-full cursor-pointer transform transition-all duration-300 ease-in-out ${activeIndex === index ? 'scale-105' : ''
+              }`}
             onClick={() => handleClick(index)} // Toggle scale animation
           >
             <h3 className="text-xl font-semibold mb-2">{event.name}</h3>
@@ -34,26 +35,51 @@ function EventSection({ title, events, gradient }) {
 }
 
 function Mes() {
- 
+
   return (
     <div>
-    <Navbar/>
-    <div className="bg-[#106B88] text-white min-h-screen">
-      <div className="container mx-auto">
-        {/* Events Section */}
-        <div className="text-center py-6">
-          <h1 className="text-4xl font-bold text-white font-serif">EVENTS</h1>
-        </div>
-        <div>
-          <EventSection title="FLAGSHIP" events={flagshipEvents} gradient="from-[#106B88] to-[#64CDC6]" />
-          <EventSection title="PAN INDIA" events={panIndiaEvents} gradient="from-[#106B88] to-[#64CDC6]" />
-          <EventSection title="MAHE" events={maheEvents} gradient="from-[#64CDC6] to-[#106B88]" />
-        </div>
+      <Navbar />
+      {/* <div className="bg-[#106B88] text-white min-h-screen">
+        <div className="container mx-auto"> */}
+          {/* Events Section */}
+          {/* <div className="text-center py-6">
+            <h1 className="text-4xl font-bold text-white font-serif">EVENTS</h1>
+          </div>
+          <div>
+            <EventSection title="FLAGSHIP" events={flagshipEvents} gradient="from-[#106B88] to-[#64CDC6]" />
+            <EventSection title="PAN INDIA" events={panIndiaEvents} gradient="from-[#106B88] to-[#64CDC6]" />
+            <EventSection title="MAHE" events={maheEvents} gradient="from-[#64CDC6] to-[#106B88]" />
+          </div> */}
 
-        {/* VC Section */}
-      </div>
-      </div>
-      <Footer/>
+          {/* VC Section */}
+        {/* </div>
+      </div> */}
+      <main>
+        <section className="py-12 bg-gradient-to-br from-blue-dark via-blue-mid to-blue-light">
+          <div className="container mx-auto px-4 md:px-10 lg:px-16">
+            <header className="mb-12">
+              <h1 className="text-4xl font-bold text-blue-pale">Flagship Events</h1>
+            </header>
+
+            <BlogList posts={flagshipEvents} />
+
+            <header className="my-12">
+              <h1 className="text-4xl font-bold text-blue-pale">Pan India</h1>
+            </header>
+
+            <BlogList posts={panIndiaEvents} />
+
+            <header className="my-12">
+              <h1 className="text-4xl font-bold text-blue-pale">MAHE</h1>
+            </header>
+
+            <BlogList posts={maheEvents} />
+
+            {/* VC Section */}
+          </div>
+        </section>
+      </main>
+      <Footer />
     </div>
   );
 }
