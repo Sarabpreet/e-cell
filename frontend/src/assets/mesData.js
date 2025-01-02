176fa4b2a6f33c4721ef307c6f89c826672b63cd
Ahmed S. <s.ahmed.profiquence@gmail.com>
2025-01-02 22:00:50 +0530

    Added images, links and now following blog page naming convention

--- frontend/src/assets/mesData.js
+++ frontend/src/assets/mesData.js
@@ -1,41 +1,57 @@
 export const flagshipEvents = [
     {
-        name: "PITCH TANK",
-        description: "💸! This is a closed door event where you 🫡🫵, students and start-ups can pitch in your ideas to our esteemed judges who happen to be venture capitalists, angel investors and alumni"
+        title: "PITCH TANK",
+        description: "💸! This is a closed door event where you 🫡🫵, students and start-ups can pitch in your ideas to our esteemed judges who happen to be venture capitalists, angel investors and alumni",
+        image: "https://d8it4huxumps7.cloudfront.net/uploads/images/opportunity/mobile_banner/67626c7789833_pitch-tank.webp",
+        postURL: "https://unstop.com/competitions/pitch-tank-manipal-entrepreneurship-summit-2025-manipal-academy-of-higher-education-mahe-karnataka-1293827"
     },
     {
-        name: "INNOVATION MELA",
-        description: "Experience the thrill of exploration as we bring together innovators, thinkers, and visionaries from diverse fields. The Innovation Mela is not just an event; it's a celebration of human creativity, pushing the boundaries of what's possible."
+        title: "INNOVATION MELA",
+        description: "Experience the thrill of exploration as we bring together innovators, thinkers, and visionaries from diverse fields. The Innovation Mela is not just an event; it's a celebration of human creativity, pushing the boundaries of what's possible.",
+        image: "https://picsum.photos/400/200",
+        postURL: "https://example.com/"
     }
 ];
 
 export const panIndiaEvents = [
     {
-        name: "CASEMAZE",
-        description: "Have a bit of interest in understanding and creating optimal solutions for companies?🤓🧐Case-Maze is here to get your brain cells running!."
+        title: "CASEMAZE",
+        description: "Have a bit of interest in understanding and creating optimal solutions for companies?🤓🧐Case-Maze is here to get your brain cells running!.",
+        image: "https://d8it4huxumps7.cloudfront.net/uploads/images/opportunity/mobile_banner/676554e771ea3_case-maze.webp",
+        postURL: "https://unstop.com/competitions/case-maze-manipal-entrepreneurship-summit-2025-manipal-academy-of-higher-education-mahe-karnataka-1303034"
     },
     {
-        name: "PROD-A-TRON",
-        description: "PROD-A-TRON provides a dynamic challenge, simulating real-world scenarios where participants must make strategic decisions for product success!📈📈"
+        title: "PROD-A-TRON",
+        description: "PROD-A-TRON provides a dynamic challenge, simulating real-world scenarios where participants must make strategic decisions for product success!📈📈",
+        image: "https://picsum.photos/400/200",
+        postURL: "https://example.com/"
     },
     {
-        name: "PROMOQUEST",
-        description: "💪 Immerse yourself in a spirited competition where you'll face off with like-minded individuals, showcase your marketing knowledge, and vie for some seriously cool prizes."
+        title: "PROMOQUEST",
+        description: "💪 Immerse yourself in a spirited competition where you'll face off with like-minded individuals, showcase your marketing knowledge, and vie for some seriously cool prizes.",
+        image: "https://picsum.photos/400/200",
+        postURL: "https://example.com/"
     }
 ];
 
 export const maheEvents = [
     {
-        name: "MONEYQUEST",
-        description: "An exhilarating event that combines the excitement of a treasure hunt with the strategic thrill of a bidding competition involving a dynamic landscape of strategy, negotiation, and resource management"
+        title: "MONEYQUEST",
+        description: "An exhilarating event that combines the excitement of a treasure hunt with the strategic thrill of a bidding competition involving a dynamic landscape of strategy, negotiation, and resource management",
+        image: "https://picsum.photos/400/200",
+        postURL: "https://example.com/"
     },
     {
-        name: "REROUTE AND RESURGE",
-        description: "Dive into the fascinating world of innovation as we present you with real problem statements derived from the setbacks of well-known startups"
+        title: "REROUTE AND RESURGE",
+        description: "Dive into the fascinating world of innovation as we present you with real problem statements derived from the setbacks of well-known startups",
+        image: "https://d8it4huxumps7.cloudfront.net/uploads/images/opportunity/mobile_banner/676560ae431ad_reroute-and-resurge.webp",
+        postURL: "https://unstop.com/competitions/reroute-and-resurge-manipal-entrepreneurship-summit-2025-manipal-academy-of-higher-education-mahe-karnataka-1303087"
     },
     {
-        name: "ENTREPREUNERIAL JACKPOT",
-        description: "A thrilling entrepreneurial game with three rounds. Accumulated tokens are crucial, leading to the grand 'Innovation Showcase Finale' where surviving teams present AI solutions for final token rewards"
+        title: "ENTREPREUNERIAL JACKPOT",
+        description: "A thrilling entrepreneurial game with three rounds. Accumulated tokens are crucial, leading to the grand 'Innovation Showcase Finale' where surviving teams present AI solutions for final token rewards",
+        image: "https://picsum.photos/400/200",
+        postURL: "https://example.com/"
     }
 ];
 
